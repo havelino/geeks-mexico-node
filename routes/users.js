bb8f4ea24ae798ada178c1d161c29264dd776b17
Hugo Alberto Avelino Hernandez <hha0009@email.wal-mart.com>
2018-03-20 20:08:32 -0600

    first commit with example

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+//simulation of our resources
+var users = [];
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(users);
+});
+//Create a new user {"user":#,"name":"something"}
+router.post('/', function(req, res, next) {
+  users.push(req.body);
+  res.json(req.body);
+});
+
+//Update an existing user {"user":#,"name":"something"}
+router.put('/', function(req, res, next) {
+  var user = users.filter(function(user){ return user.user==req.body.user}).pop();//find the user
+  if(user!= null && user!= undefined){
+    console.log("user recovered is:",user);
+    user.name=req.body.name;//update name by reference
+    res.json(user);
+  }else{
+    res.status(404);
+    res.json("User to update not found");
+  }
+  
+});
+
+module.exports = router;
